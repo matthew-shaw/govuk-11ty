611906198552b8b11bab13470052e6bcb1948bc7
Matt Shaw <matthew.shaw@landregistry.gov.uk>
2025-06-12 15:34:54 +0100

    debranding config

--- eleventy.config.js
+++ eleventy.config.js
@@ -2,7 +2,38 @@ import { govukEleventyPlugin } from "@x-govuk/govuk-eleventy-plugin";
 
 export default function (eleventyConfig) {
   // Register the plugin
-  eleventyConfig.addPlugin(govukEleventyPlugin);
+  eleventyConfig.addPlugin(govukEleventyPlugin, {
+    rebrand: true,
+    titleSuffix: "HM Land Registry",
+    header: {
+      logotype: {
+        text: "HM Land Registry",
+      },
+    },
+    serviceNavigation: {
+      serviceName: "Business Gateway",
+      serviceUrl: "#",
+      navigation: [
+        {
+          href: "#",
+          text: "Navigation item 1",
+        },
+        {
+          href: "#",
+          text: "Navigation item 2",
+          active: true,
+        },
+        {
+          href: "#",
+          text: "Navigation item 3",
+        },
+      ],
+    },
+    footer: {
+      logo: false,
+    },
+    stylesheets: ["/assets/styles.css"],
+  });
 
   return {
     dataTemplateEngine: "njk",
